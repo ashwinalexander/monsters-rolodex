48104659042aa05bfd8d6561f166ba7534fb5114
ashwin <alexwin75@gmail.com>
2020-10-01 11:44:00 -0400

    Updates

---
+++ precache-manifest.1b1c754d565494680a0d9917bdb0859e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd4871daa23600b433a53d059aad962",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ab10a3e1f28e0287773e",
+    "url": "/monsters-rolodex/static/css/main.a9a7cefb.chunk.css"
+  },
+  {
+    "revision": "564e6141b86eff7955cc",
+    "url": "/monsters-rolodex/static/js/2.26f67bcd.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters-rolodex/static/js/2.26f67bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab10a3e1f28e0287773e",
+    "url": "/monsters-rolodex/static/js/main.f871c911.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
